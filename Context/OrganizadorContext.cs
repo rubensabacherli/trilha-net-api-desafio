40c7ad87e704879bd7cf82f4ac24630edcf8ef50
Rubens Abacherli Neto <rubensabacherli@gmail.com>
2024-08-22 21:07:16 -0300

    exercício prático de API

--- Context/OrganizadorContext.cs
+++ Context/OrganizadorContext.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using TrilhaApiDesafio.Models;
 
@@ -5,11 +9,7 @@ namespace TrilhaApiDesafio.Context
 {
     public class OrganizadorContext : DbContext
     {
-        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
-        {
-            
-        }
-
+        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options) { }
         public DbSet<Tarefa> Tarefas { get; set; }
     }
 }
